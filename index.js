8bb65da6a622bddf40432abb6ee4817e8d1d5853
Juan Carlos Patron <charliepatronr@gmail.com>
2019-12-30 22:58:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,3 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 
-function destructivelyAppendKitten(name){
-  kittens.pop();
-   kittens.shift();
-   kittens.push(name);
-   return kittens;
-  
-}
