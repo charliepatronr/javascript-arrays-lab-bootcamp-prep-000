b2cecbf803be97ad789117433a264a3e859a5105
Juan Carlos Patron <charliepatronr@gmail.com>
2019-12-30 22:52:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+
+function destructivelyAppendKitten(name){
+  kittens.pop(name);
+  
+}
